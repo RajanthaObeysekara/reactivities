b9e0dc6ec10584b988d1adab753b04771625f1fd
rajantha22 <hncrasteam@gmail.com>
2021-12-05 19:42:00 +0530

    end of section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    { text: 'Drinks', value: 'drinks',key:'drinks' },
+    { text: 'Culture', value: 'culture',key:'culture' },
+    { text: 'Film', value: 'film',key:'film' },
+    { text: 'Food', value: 'food',key:'food' },
+    { text: 'Music', value: 'music',key:'music' },
+    { text: 'Travel', value: 'travel',key:'travel' }, 
+]
